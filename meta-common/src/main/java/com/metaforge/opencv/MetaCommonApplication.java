a33cfe2ca43fe6ab4bfbfb7c6bb473aee847422f
pengjl <boonya@163.com>
2026-02-27 15:28:11 +0800

    add common model.

---
+++ meta-common/src/main/java/com/metaforge/opencv/MetaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.metaforge.opencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaCommonApplication.class, args);
+    }
+
+}
---
+++ meta-common/src/test/java/com/metaforge/opencv/MetaCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.metaforge.opencv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+
+public class MetaCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
